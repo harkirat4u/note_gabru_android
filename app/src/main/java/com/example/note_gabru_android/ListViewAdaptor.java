fd1b2cc210995f0d156b99cb1c3a7838d58fb4f1
diljit468 <62748421+diljit468@users.noreply.github.com>
2020-07-18 06:18:11 -0400

    list

--- app/src/main/java/com/example/note_gabru_android/ListViewAdaptor.java
+++ app/src/main/java/com/example/note_gabru_android/ListViewAdaptor.java
@@ -1,4 +1,16 @@
 package com.example.note_gabru_android;
 
-public class ListViewAdaptor {
+
+public class ListViewAdaptor extends ArrayAdapter {
+
+
+    Context mContext;
+    int layoutRes;
+    List<String> categories;
+    public ListViewAdaptor(@NonNull Context context, int resource,List<String> category) {
+        super(context, resource,category);
+        this.mContext = context;
+        this.layoutRes = resource;
+        this.categories = category;
+    }
 }
