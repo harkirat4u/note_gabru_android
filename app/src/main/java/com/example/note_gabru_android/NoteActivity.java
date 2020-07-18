e7774d32a2d539e27f7427aa4d2c04b22325fadc
RamSinghiggan <57167406+RamSinghiggan@users.noreply.github.com>
2020-07-18 03:29:01 -0400

    note1

--- app/src/main/java/com/example/note_gabru_android/NoteActivity.java
+++ app/src/main/java/com/example/note_gabru_android/NoteActivity.java
@@ -1,4 +1,101 @@
 package com.example.note_gabru_android;
 
-public class NoteActivity {
-}
+    
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private GoogleMap mMap;
+    Marker homeMarker;
+    Marker destMarker;
+    public int i;
+    private final int REQUEST_CODE = 1;
+    // finding the user location
+    private FusedLocationProviderClient mFusedLocationClient;
+    LocationCallback locationCallback;
+    LocationRequest locationRequest;
+
+    public final int RADIUS = 1500;
+    double latitude, longitude;
+    double dest_lat, dest_lng;
+    // boolean test
+    public static boolean directionRequested;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Location");
+        initMap();
+        getUserLocation();
+
+        if (!checkPermissions())
+            requestPermissions();
+        else {
+            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+        }
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if (mapFragment == null) {
+            System.out.println("error null");
+        } else {
+            System.out.println("every thing is fine");
+        }
+
+        mapFragment.getMapAsync(this);
+    }
+
+    private void getUserLocation() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+//        locationRequest.setSmallestDisplacement(10);
+        setHomeMarker();
+    }
