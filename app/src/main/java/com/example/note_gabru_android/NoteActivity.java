34488e3648d7d6d5eca96c7fcf5f7b0aef9ff9fc
RamSinghiggan <57167406+RamSinghiggan@users.noreply.github.com>
2020-07-18 06:27:09 -0400

    note display

--- app/src/main/java/com/example/note_gabru_android/NoteActivity.java
+++ app/src/main/java/com/example/note_gabru_android/NoteActivity.java
@@ -1,4 +1,41 @@
 package com.example.note_gabru_android;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class NoteActivity {
-}
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotesActivity extends AppCompatActivity {
+    GridView gridView;
+    FloatingActionButton floatingActionButton;
+    DataBaseHelper dataBaseHelper;
+    String audioPath;
+    int ccid;
+    SearchView searchView;
+    List<CategoryModel> filterList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Notes");
+    }
