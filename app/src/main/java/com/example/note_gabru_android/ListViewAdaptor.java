8da9403809c8bcad323826ea57882fa4d4b48ddb
harkirat singh <singhharkirat051@gmail.com>
2020-07-18 06:14:55 -0400

    activities

--- app/src/main/java/com/example/note_gabru_android/ListViewAdaptor.java
+++ app/src/main/java/com/example/note_gabru_android/ListViewAdaptor.java
@@ -1,4 +1,4 @@
 package com.example.note_gabru_android;
 
-public class NoteActivity {
+public class ListViewAdaptor {
 }
---
+++ app/src/main/java/com/example/note_gabru_android/MapActivity.java
@@ -0,0 +1,255 @@
+package com.example.note_gabru_android;
+
+    
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private GoogleMap mMap;
+    Marker homeMarker;
+    Marker destMarker;
+    public int i;
+    private final int REQUEST_CODE = 1;
+    // finding the user location
+    private FusedLocationProviderClient mFusedLocationClient;
+    LocationCallback locationCallback;
+    LocationRequest locationRequest;
+
+    public final int RADIUS = 1500;
+    double latitude, longitude;
+    double dest_lat, dest_lng;
+    // boolean test
+    public static boolean directionRequested;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Location");
+        initMap();
+        getUserLocation();
+
+        if (!checkPermissions())
+            requestPermissions();
+        else {
+            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+        }
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if (mapFragment == null) {
+            System.out.println("error null");
+        } else {
+            System.out.println("every thing is fine");
+        }
+
+        mapFragment.getMapAsync(this);
+    }
+
+    private void getUserLocation() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+//        locationRequest.setSmallestDisplacement(10);
+        setHomeMarker();
+    }
+    
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        Intent intent = getIntent();
+
+
+        dest_lat = intent.getDoubleExtra("latitude", 0);
+        dest_lng = intent.getDoubleExtra("longitude", 0);
+
+        LatLng noteLatLng = new LatLng(dest_lat, dest_lng);
+        MarkerOptions options = new MarkerOptions().position(noteLatLng).title("your saved loaction")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        ;
+        mMap.addMarker(options);
+
+    }
+
+    private void setMarker(Location location) {
+        LatLng userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions options = new MarkerOptions().position(userLatLng)
+                .title("Your Destination")
+                .snippet("You are going there")
+                .draggable(true);
+        destMarker = mMap.addMarker(options);
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+
+    private String getDirectionUrl() {
+        StringBuilder googleDirectionUrl = new StringBuilder("https://maps.googleapis.com/maps/api/directions/json?");
+        googleDirectionUrl.append("origin=" + latitude + "," + longitude);
+        googleDirectionUrl.append("&destination=" + dest_lat + "," + dest_lng);
+        googleDirectionUrl.append("&key=" + getString(R.string.api_key_places));
+        Log.d("", "getDirectionUrl: " + googleDirectionUrl);
+        return googleDirectionUrl.toString();
+    }
+
+    private String getUrl(double latitude, double longitude, String nearbyPlace) {
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlaceUrl.append("location=" + latitude + "," + longitude);
+        googlePlaceUrl.append("&radius=" + RADIUS);
+        googlePlaceUrl.append("&type=" + nearbyPlace);
+        googlePlaceUrl.append("&key=" + getString(R.string.api_key_places));
+        Log.d("", "getUrl: " + googlePlaceUrl);
+        return googlePlaceUrl.toString();
+
+    }
+
+
+    private void setHomeMarker() {
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    latitude = userLocation.latitude;
+                    longitude = userLocation.longitude;
+                    if (homeMarker != null)
+                        homeMarker.remove();
+//                    mMap.clear(); //clear the old markers
+                    CameraPosition cameraPosition = CameraPosition.builder()
+                            .target(new LatLng(userLocation.latitude, userLocation.longitude))
+                            .zoom(10)
+                            .bearing(0)
+                            .tilt(45)
+                            .build();
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                    homeMarker = mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location")
+                            .icon(bitmapDescriptorFromVector(getApplicationContext(), R.drawable.icon_loc)));
+
+                }
+            }
+        };
+    }
+    
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setHomeMarker();
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
+            }
+        }
+    }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.map_activity,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Object[] dataTransfer;
+        switch (item.getItemId()){
+
+            case R.id.btn_duration:
+
+            case R.id.btn_direction:
+                dataTransfer = new Object[3];
+             String   url = getDirectionUrl();
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+                dataTransfer[2] = new LatLng(dest_lat, dest_lng);
+
+                GetDirectionsData getDirectionsData = new GetDirectionsData();
+                getDirectionsData.execute(dataTransfer);
+                if (item.getItemId() == R.id.btn_duration)
+                    directionRequested = false;
+                else
+                    directionRequested = true;
+                break;
+        }
+        return true;
+    }
+
+
+
+}
+
