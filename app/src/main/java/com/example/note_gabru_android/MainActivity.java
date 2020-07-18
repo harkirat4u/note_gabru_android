13eb65928b98766de686b94a384741a4ee3ac428
RamSinghiggan <57167406+RamSinghiggan@users.noreply.github.com>
2020-07-18 03:41:30 -0400

    complete

--- app/src/main/java/com/example/note_gabru_android/MainActivity.java
+++ app/src/main/java/com/example/note_gabru_android/MainActivity.java
@@ -57,3 +57,59 @@ public class MainActivity extends AppCompatActivity {
         }
         adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,categoryName);
         listView.setAdapter(adapter);
+        addCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+               View view = inflater.inflate(R.layout.category_dialog,null);
+               builder.setView(view);
+                final EditText editTextCategory = view.findViewById(R.id.edit_text_cat);
+                builder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String cat = editTextCategory.getText().toString();
+
+                        categoryName.add(cat);
+
+                            try {
+                                sharedPreferences.edit().putString("cname",ObjectSerializer.serialize(categoryName)).apply();
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            listView.setAdapter(adapter);
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+               dialog.show();
+            }
+        });
+
+     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+         @Override
+         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+             catPosition = position;
+             Intent intent = new Intent(MainActivity.this, NotesActivity.class);
+             intent.putExtra("id", position);
+             startActivity(intent);
+
+         }
+     });
+
+
+
+    }
+
+
+}
+
